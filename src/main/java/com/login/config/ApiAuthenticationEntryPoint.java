113b217ab5d9ab79914233c723c85be5585b35cb
Jacopo <jacoposabatini76@gmail.com>
2023-01-09 17:21:15 +0100

    Update ApiAuthenticationEntryPoint.java
    fix log

--- src/main/java/com/login/config/ApiAuthenticationEntryPoint.java
+++ src/main/java/com/login/config/ApiAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
@@ -17,6 +18,7 @@ import java.io.IOException;
  * Questa classe restituisce semplicemente il codice di risposta HTTP 401 Non autorizzato quando l'autenticazione non riesce,
  * sovrascrivendo il reindirizzamento predefinito di Spring.
  */
+@Slf4j
 @Component
 public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
