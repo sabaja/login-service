705b078763f29c4fc2186d9eaeb5075e318c15b0
sabaja <jacoposabatini76@gmail.com>
2023-01-11 22:21:54 +0100

    Fix import

--- src/main/java/com/login/config/ApiAuthenticationEntryPoint.java
+++ src/main/java/com/login/config/ApiAuthenticationEntryPoint.java
@@ -3,10 +3,11 @@ package com.login.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
